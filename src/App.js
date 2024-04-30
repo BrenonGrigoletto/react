d99c4ff096a548a0ae2ce0698f24ba784c022ad6
Brenon Greigoletto <113072662+BrenonGrigoletto@users.noreply.github.com>
2024-04-30 00:29:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import Relogio from './Relogio';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function Letreiro() {
+  const tex = "Conheça a Fatec! ";
+  const [digitacao, digitacaoTexto] = useState('');
+
+  useEffect(() => {
+    let index = 0;
+
+    const intervalo = setInterval(() => {
+      digitacaoTexto((conteudo) => {
+        index = (index + 1) % tex.length;
+        return tex.substring(0, index);
+      });
+    }, 707);
+
+    return () => clearInterval(intervalo);
+  }, [tex]);
+
   return (
-    <div className="App">
-      <Relogio />
-      {
-        
-      }
+    <div>
+      <h1>{digitacao}</h1>
     </div>
   );
 }
 
-export default App;
+export default Letreiro;
